0d62f4d2e6fb010e7b4b6a8a02b456b79f315076
SergioFi666 <sergio.figueroa2304@gmail.com>
2020-05-19 18:30:34 +0200

    Traduccion privacidad y terminos

--- resources/views/termes.blade.php
+++ resources/views/termes.blade.php
@@ -5,50 +5,52 @@
 <section>
     <div class="container">
         <h2 style="text-align: center;">
-            <strong>Termes i Condicions d'Ús</strong>
+            <strong>TÉRMINOS Y CONDICIONES DE USO</strong>
         </h2>
         <p>&nbsp;</p>
         <p>
-            <strong>INFORMACIÓ RELLEVANT</strong>
+            <strong>Información Relevante</strong>
         </p>
-        <p>És requisit necessari per a l'adquisició dels productes que s'ofereixen en aquest lloc, que llegeixi i accepti els següents termes i condicions que a continuació es redacten. L'ús dels nostres serveis així com la compra dels nostres productes implicarà que vostè ha llegit i acceptat els Termes i Condicions d'Ús en el present document. Tots els productes que són oferts pel nostre lloc web poguessin ser creades, cobrades, enviades o presentades per una pàgina web tercera i en aquest cas estarien subjectes als seus propis termes i condicions. En alguns casos, per adquirir un producte, serà necessari el registre per part de l'usuari, amb ingrés de dades personals fidedignes i definició d'una contrasenya.</p>
-        <p>L'usuari pot triar i canviar la clau per al seu accés d'administració del compte en qualsevol moment, en cas que s'hagi registrat i que sigui necessari per a la compra d'algun dels nostres productes. www.debuggers.cat no assumeix la responsabilitat en cas que lliuri la clau a tercers.</p>
+        <p>Es requisito necesario para la adquisición de los productos que se ofrecen en este lugar, que lea y acepte los siguientes termas y condiciones que a continuación se redactan. El uso de nuestros servicios así como la compra de nuestros productos implicará que usted ha leído y aceptado los Términos y Condiciones de Uso en el presente documento. Todos los productos que son ofrecidos por nuestro sitio web pudieran ser creadas, cobradas, enviadas o presentadas por una página web tercera y en este caso estarían sujetos a sus propios términos y condiciones. En algunos casos, para adquirir un producto, será necesario el registro por parte del usuario, con ingreso de datos personales fidedignos y definición de una contraseña.</p>
+        <p>El usuario puede elegir y cambiar la clave para su acceso de administración de la cuenta en cualquier momento, en caso de que se haya registrado y que sea necesario para la compra de alguno de nuestros productos. <strong>tornejos.debuggers.cat</strong> no asume la responsabilidad en caso de que libre la clave a terceros.</p>
         <p>Todas las compras y transacciones que se lleven a cabo por medio de este sitio web, están sujetas a un proceso de confirmación y verificación, el cual podría incluir la verificación del stock y disponibilidad de producto, validación de la forma de pago, validación de la factura (en caso de existir) y el cumplimiento de las condiciones requeridas por el medio de pago seleccionado. En algunos casos puede que se requiera una verificación por medio de correo electrónico.</p>
-        <p>Totes les compres i transaccions que es duguin a terme per mitjà d'aquest lloc web, estan subjectes a un procés de confirmació i verificació, el qual podria incloure la verificació de l'estoc i disponibilitat de producte, validació de la forma de pagament, validació de la factura (en cas d'existir) i el compliment de les condicions requerides pel mitjà de pagament seleccionat. En alguns casos pot ser que es requereixi una verificació per mitjà de correu electrònic.</p>
         <p>
-            <strong>LLICÈNCIA</strong>
+            <strong>Licéncia</strong>
         </p>
-        <p>Debuggers &nbsp;a través del seu lloc web concedeix una llicència perquè els usuaris utilitzin &nbsp;els productes que són venuts en aquest lloc web d'acord amb els termes i condicions que es descriuen en aquest document.</p>
+        <p>Debuggers &nbsp;a través de su sitio web concede una licencia porque los usuarios utilicen &nbsp;los productos que son vendidos en este sitio web de acuerdo con los términos y condiciones que se describen en este documento.</p>
         <p>
-            <strong>ÚS NO AUTORITZAT</strong>
+            <strong>Uso No autorizado</strong>
         </p>
-        <p>En cas que apliqui (per a venda de programari, templets, o un altre producte de disseny i programació) no pots col·locar un dels nostres productes, modificat o sense modificar, en un CD, lloc web o cap altre mitjà i oferir-los per la redistribució o la revenda de cap classe.</p>
+        <p>En caso de que aplique (para venta de software, templetes, u otro producto de diseño y programación) no puedes colocar uno de nuestros productos, modificado o sin modificar, en un CD, sitio web o jefe otro mediano y ofrecerlos por la redistribución o la reventa de ninguna clase.</p>
         <p>
-            <strong>PROPIETAT</strong>
+            <strong>Propiedad</strong>
         </p>
-        <p>No pot declarar propietat intel·lectual o exclusiva a cap dels nostres productes, modificat o sense modificar. Tots els productes són propietat &nbsp;dels proveïdors del contingut. En cas que no s'especifiqui el contrari, els nostres productes es proporcionen &nbsp;sense cap mena de garantia, expressa o implícita. En cap aquesta companyia serà &nbsp;responsables de cap dany incloent, però no limitat a, danys directes, indirectes, especials, fortuïts o conseqüents o altres pèrdues resultants de l'ús o de la impossibilitat d'utilitzar els nostres productes.</p>
+        <p>No puede declarar propiedad intelectual o exclusiva a ninguno de nuestros productos, modificado o sin modificar. Todos los productos son propiedad &nbsp;de los proveedores del contenido. En caso de que no se especifique el contrario, nuestros productos se proporcionan &nbsp; sin ningún tipo de garantía, exprés o implícita. cabe esta compañía será &nbsp;responsables de ningún daño incluyente, pero no limitado a, daños directos, indirectos, especiales, fortuitos o consecuentes u otras pérdidas resultantes del uso o de la imposibilidad de utilizar nuestros productos.
+</p>
         <p>
-            <strong>POLÍTICA DE REEMBORSAMENT I GARANTIA</strong>
+            <strong>Política De Reembolso I Garantia</strong>
         </p>
-        <p>En el cas de productes que siguin &nbsp;mercaderies irrevocables no-tangibles, no realitzem reemborsaments després que s'enviï el producte, vostè té la responsabilitat d'entendre abans de comprar-lo. &nbsp;Li demanem que llegiu amb atenció abans de comprar-lo. Fem només excepcions amb aquesta regla quan la descripció no s'ajusta al producte. Hi ha alguns productes que poguessin tenir garantia i possibilitat de reemborsament però aquest serà especificat en comprar el producte. En aquests casos la garantia només cobrirà falles de fàbrica i només es farà efectiva quan el producte s'hagi fet servir correctament. La garantia no cobreix avaries o danys ocasionats per ús indegut. Els termes de la garantia estan associats a falles de fabricació i funcionament en condicions normals dels productes i només es faran efectius aquests termes si l'equip ha estat usat correctament. Això inclou:</p>
+        <p>En el caso de productos que sean &nbsp;mercancías irrevocables no-tangibles, no realizamos reembolsos después de que se envíe el producto, usted tiene la responsabilidad de entender antes de comprarlo. &nbsp;Le pedimos que leáis con atención antes de comprarlo. Basura solo excepciones con esta regla cuando la descripción no se ajusta al producto. Hay algunos productos que pudieran tener garantía y posibilidad de reembolso pero este será especificado al comprar el producto. En estos casos la garantía solo cubrirá fallas de fábrica y solo se hará efectiva cuando el producto se haya usado correctamente. La garantía no cubre averías o daños ocasionados por uso indebido. Los términos de la garantía están asociados a fallas de fabricación y funcionamiento en condiciones normales de los productos y solo se harán efectivos estos términos si el equipo ha sido usado correctamente. Esto incluye:</p>
         <p>
-            – D'acord amb les especificacions tècniques indicades per a cada producte.<br>
-            – En condicions ambientals d'acord amb les especificacions indicades pel fabricant.<br>
-            – En ús específic per a la funció amb què va ser dissenyat de fàbrica.<br>– En condicions d'operació elèctriques d'acord amb les especificacions i toleràncies indicades.
-        </p>
-        <p>
-            <strong>COMPROVACIÓ ANTIFRAU</strong>
-        </p>
-        <p>La compra del client pot ser ajornada per a la comprovació antifrau. També pot ser suspesa per més temps per a una investigació més rigorosa, per evitar transaccions fraudulentes.</p>
-        <p>
-            <strong>PRIVACITAT</strong>
+            – De acuerdo con las especificaciones técnicas indicadas para cada producto.<br>
+            – En condiciones ambientales de acuerdo con las especificaciones indicadas por el fabricante.<br>
+            – En uso específico para la función con qué fue diseñado de fábrica.<br>
+            – En condiciones de operación eléctricas de acuerdo con las especificaciones y tolerancias indicadas.
         </p>
         <p>
-            Aquest <a href="http://torneos.debuggers.cat" target="_blank">lloc web</a>
-            www.debuggers.cat garanteix que la informació personal que vostè envia compta amb la seguretat necessària. Les dades ingressades per usuari o en el cas de requerir una validació de les comandes no seran lliurats a tercers, llevat que hagi de ser revelada en compliment a una ordre judicial o requeriments legals.
+            <strong>Comprobación Antifraude</strong>
         </p>
-        <p>La subscripció a butlletins de correus electrònics publicitaris és voluntària i podria ser seleccionada a l'hora de crear el seu compte.</p>
-        <p>Debuggers reserva els drets de canviar o de modificar aquests termes sense previ avís.</p>
+        <p>La compra del cliente puede ser aplazada para la comprobación antifraude. También puede ser suspendida por más tiempo para una investigación más rigurosa, para evitar transacciones fraudulentas.</p>
+        <p>
+            <strong>Privacidad</strong>
+        </p>
+        <p>
+            Este <a href="http://tornejos.debuggers.cat" target="_blank">sitio web</a>
+            <strong>tornejos.debuggers.cat</strong> garantiza que la información personal que usted envía cuenta con la seguridad necesaria. Los datos ingresados por usuario o en el caso de requerir una validación de los pedidos no serán librados a terceros, salvo que tenga que ser revelada en cumplimiento a una orden judicial o requerimientos legales.
+
+        </p>
+        <p>La suscripción a boletines de correos electrónicos publicitarios es voluntaria y podría ser seleccionada en la hora de crear su cuenta.</p>
+        <p>Debuggers reserva los derechos de cambiar o de modificar estos términos sin previo aviso.</p>
 
     </div>
 </section>
